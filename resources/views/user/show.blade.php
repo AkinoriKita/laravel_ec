f220e42192e672b1a27e36879fa5a6a74f09adbd
AkinoriKita <aknor1004@gmail.com>
2022-09-29 10:29:25 +0900

    [fix]画像表示、ログイン登録日本語化

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -11,7 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="md:flex md:justify-around">
                         <div class="md:w-1/2 ml-4">
-                            <img alt="ecommerce" class="object-cover object-center block mx-auto mb-8" src="https://dummyimage.com/420x260">
+                            @if ($product->filename === null || $product->filename === "")
+                            <img alt="ecommerce" class="mx-auto mb-8" src="https://dummyimage.com/420x260">
+                            @else
+                            <img class="mx-auto mb-8" src="{{ asset($product->filepath) }}">
+                            @endif
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">{{ $product->name }}</h1>
                             <p class="leading-relaxed mb-8">{{ $product->information }}</p>
                             <div class="md:flex justify-around items-center">
@@ -20,6 +24,12 @@
                                 </div>
                                 <form action="{{ route('user.cart.add') }}" method="post">
                                 @csrf
+                                @if ($quantity === 0)
+                                <div class="items-center">
+                                    <p class="text-right text-red-500 mb-1">在庫がありません！</p>
+                                    <button disabled class="flex pointer-events-none ml-16 text-white bg-gray-500 border-0 py-2 px-6 focus:outline-non rounded">カートに入れる</button>
+                                </div>
+                                @else
                                 <div class="flex items-center">
                                     <span class="mr-3">数量</span>
                                     <div class="relative">
@@ -32,6 +42,8 @@
                                     <button class="flex ml-16 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
                                     <input type="hidden" name="product_id" value="{{ $product->id}}"> 
                                 </div>
+                                @endif
+                                
                             </form>
                             </div>
                         </div>
